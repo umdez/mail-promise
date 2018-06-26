5b16fc581308e98cb5f26e98da2fbedb941a1d3e
seanpmaxwell <seanpmaxwell1@gmail.com>
2018-06-26 00:07:56 -0700

    second commit

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,8 @@
 /**
- *
+ * Type definition file
  *
  * created by sean maxwell June 25, 2018
  */
-
-
-// declare class MailPromise {
-//     constructor(service: string, username: string, pwd: string);
-//     send(from: string, to: string, subject: string, text: string, html?: string): Promise<any>;
-// }
-
-
-// Pick up here, next to export a function which returns an object literal
 
 
 declare module MailPromise {
