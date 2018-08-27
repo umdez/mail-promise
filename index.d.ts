abbc384f48f7e423a7b6621251d8cc0e40352263
seanpmaxwell <seanpmaxwell1@gmail.com>
2018-08-26 18:17:42 -0700

    updated to typescript

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-export default class MailPromise {
-    private service;
-    private username;
-    private pwd;
-    constructor(service: string, username: string, pwd: string);
-    send(to: string, from: string, subject: string, text: string, html?: string): any;
-}
